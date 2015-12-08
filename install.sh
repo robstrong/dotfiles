0b9cf410413a0b6c58a7e12bf275a6c5eee1bf0f
Rob Strong <rstrong@pica9.com>
2015-12-08 11:24:11 -0500

    make symlink before vundle install

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ git clone git@github.com:robstrong/dotfiles ~/.dotfiles
 cd ~/.dotfiles
 git init
 git submodule update
+ln -s ~/.dotfiles/vim ~/.vim
 vim +PluginInstall +qall
 ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
-ln -s ~/.dotfiles/vim ~/.vim
 ln -s ~/.dotfiles/zshrc ~/.zshrc

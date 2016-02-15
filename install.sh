3e93fbe270f4234dddd93278102675bd0be1dd38
Rob Strong <robert.a.strong@gmail.com>
2016-02-15 14:32:48 -0500

    update dep install, add git config

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 
+sudo apt-get install -y git vim zsh
 git clone --recursive git@github.com:robstrong/dotfiles ~/.dotfiles
 cd ~/.dotfiles
 git init
@@ -9,3 +10,5 @@ ln -s ~/.dotfiles/vim ~/.vim
 vim +PluginInstall +qall
 ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
 ln -s ~/.dotfiles/zsh/zshrc ~/.zshrc
+ln -s ~/.dotfiles/git/gitconfig ~/.gitconfig
+chsh -s $(which zsh)

043daef335ab5a732ed2aa0531e615e0b439cfee
Rob Strong <robert.a.strong@gmail.com>
2016-09-07 17:09:02 -0400

    cleaning up

--- fabfile.py
+++
@@ -1,8 +0,0 @@
-from fabric.api import *
-from fabric.context_managers import cd
-
-env.use_ssh_config = True
-
-def install():
-    run('sh -c "$(curl -fsSL https://raw.github.com/robstrong/dotfiles/master/install.sh)"')
-    put('~/.wakatime.cfg', '~/.wakatime.cfg')

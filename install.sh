a61307980b31d7dee0e84edbfbde82a2aeb20acb
Rob Strong <robert.a.strong@gmail.com>
2018-01-31 07:48:51 -0500

    remove neovim option

--- install.sh
+++ install.sh
@@ -13,10 +13,5 @@ vim +PluginInstall +qall
 ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
 ln -s ~/.dotfiles/zsh/zshrc ~/.zshrc
 ln -s ~/.dotfiles/git/gitconfig ~/.gitconfig
-if [ -d ~/.config ]; then
-    # allow neovim config as well
-    ln -s ~/.vim ~/.config/nvim
-    ln -s ~/.vimrc ~/.config/nvim/init.vim
-fi
 
 chsh -s $(which zsh)
